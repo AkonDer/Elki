0ca0350a6f4b3e8264e25fdfb3b11a1a8cd579f8
Ivan <van.koshkin.fr@gmail.com>
2021-11-18 14:42:58 +0300

    Finish OOP

--- Program.cs
+++ Program.cs
@@ -1,36 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Timers;
-using Timer = System.Timers.Timer;
 
 namespace Elki
 {
     internal class Program
     {
-        private static Timer _aTimerElki;
-        private static Timer _aTimerBd;
-        private static Timer _aTimerClock;
-        private static Timer _aTimerHolidays;
-        private static string[] _dataElki;
-        private static readonly List<Employee> Employees = new List<Employee>();
-        private static readonly List<Holiday> Holidays = new List<Holiday>();
-        private static decimal _lists;
-        private static decimal _whichlist = 1;
-        /// <summary>
-        /// Какая дата сегодня
-        /// </summary>
-        public static string dataNow;
-
         private static void Main()
         {
             // start pogram
-            Console.WriteLine("Start programm in " + DateTime.Now);            
+            Console.WriteLine("Start programm in " + DateTime.Now);
 
             List<ElkiTimer> timers = new List<ElkiTimer>()
             {
