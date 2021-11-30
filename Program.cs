aa7cd80dcfa1c877292ac64e4fd5203308e7fe1c
Ivan <van.koshkin.fr@gmail.com>
2021-11-30 08:36:35 +0300

    v 0.0.1

--- Program.cs
+++ Program.cs
@@ -9,11 +9,7 @@ namespace Elki
     internal class Program
     {
         private static void Main()
-        {
-            //WeatherScrap webScrapper = new WeatherScrap("https://www.gismeteo.ru/weather-fryazino-12648/");
-            //webScrapper.RunScrapper();
-            //File.WriteAllBytes("image.jpg", webScrapper.Image);
-
+        {     
             // start pogram
             Console.WriteLine("Start programm in " + DateTime.Now);
 
